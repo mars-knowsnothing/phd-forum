23c0a96e8e4057f3ea40887eeba1b5f6e65cbec3
李若然 <mars@liruorans-MacBook-Pro.local>
2024-09-20 09:08:31 +0800

    1. fix image issue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -20,10 +20,10 @@ export default {
     return {
       images: [
         // 使用相对路径加载图片
-        './assets/image_1.jpg',
-        './assets/image_2.jpg',
-        './assets/image_3.jpg',
-        './assets/image_4.jpg',
+        '/assets/image_1.jpg',
+        '/assets/image_2.jpg',
+        '/assets/image_3.jpg',
+        '/assets/image_4.jpg',
       ],
     };
   },
